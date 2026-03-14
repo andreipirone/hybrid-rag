4d219c91dcbfce98c00b04ebf6c196b9f7c0932a
Andrei Pirone <andrei.pirone@gmail.com>
2026-03-14 22:05:23 +0200

    implemented text splitter

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-file_path = "./pdf/spring-start-here.pdf"
+file_path = "./pdf/the-stranger-albert-camus.pdf"
 loader = PyPDFLoader(file_path)
 
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, 
+    chunk_overlap=200,  
+    add_start_index=True, 
+)
+
 docs = loader.load()
-print(docs[44])
+print(len(docs))
+split_docs = text_splitter.split_documents(docs)
+print(len(split_docs))
+
+for chunk in split_docs:
+    print(chunk)
