855312901cf7396030e374575671cc10675de1b1
Andrei Pirone <andrei.pirone@gmail.com>
2026-03-16 20:52:07 +0200

    add retrieval

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import ChatOllama
 from langchain_qdrant import QdrantVectorStore
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from uuid import uuid4
 
 file_path = "./pdf/hyperpolyglot-handbook.pdf"
@@ -27,10 +31,46 @@ qdrant = QdrantVectorStore.from_documents(
     embeddings,
     ids = uuids,
     url="http://localhost:6333",
+    prefer_grpc=True,
     collection_name="my_documents",
 )
 
-found_docs = qdrant.similarity_search("What are the top reasons to learn a language")
-for found in found_docs:
-    print(found)
+retriever = qdrant.as_retriever(k=3)
+# found_docs = retriever._get_relevant_documents()
+
+# found_docs = qdrant.similarity_search("What are the top reasons to learn a language")
+
+# for docs in found_docs:
+#     print(docs.page_content)
+#     print(docs.metadata)
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+template = [("human", """You are an assistant for question-answering tasks. 
+                    Use the following pieces of retrieved context to answer the question. 
+                    If you don't know the answer, just say that you don't know. 
+                    Use three sentences maximum and keep the answer concise.
+                    Question: {question} 
+                    Context: {context} 
+                    Answer:""")]
+
+prompt = ChatPromptTemplate(template)
+
+llm = ChatOllama(model="gemma3:1b", temperature=0.5)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+query = input("> ")
+
+response = rag_chain.invoke(query)
+print(response)
+
+
+
 
